a20555c9b65f720986c70ec8b09578cfdf8bd119
Corruptsive <gabriel.r.hein@outlook.com>
2026-08-15 08:22:56 -0700

    colliders

--- scripts/cactus.gd
+++ scripts/cactus.gd
@@ -6,12 +6,12 @@ func _ready() -> void: # runs once and void means it reutns nothing
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	print(collider.get_overlapping_areas())
-	var overlaps = collider.get_overlapping_areas()
-	for i in overlaps:
-		if i.name == "playerArea":
-			globals.health = 0
+#func _process(delta: float) -> void:
+	#print(collider.get_overlapping_areas())
+	#var overlaps = collider.get_overlapping_areas()
+	#for i in overlaps:
+		#if i.name == "playerArea":
+			#globals.health = 0
 		#globals.health = 0
 	
 	
