e3c156efc2b9f92b3948637e844a3ecd923b8548
Corruptsive <gabriel.r.hein@outlook.com>
2026-08-15 04:44:27 -0700

    moving

--- scripts/player-handler.gd
+++ scripts/player-handler.gd
@@ -8,5 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var movement: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up") * speed
+	var movement: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * speed
 	position += movement
