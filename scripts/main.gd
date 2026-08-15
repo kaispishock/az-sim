dff41900a2d90f3a7e72cf38c8e2fa287803c0cf
Corruptsive <gabriel.r.hein@outlook.com>
2026-08-15 10:22:09 -0700

    :3

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,13 +3,12 @@ extends Node2D
 var cactus = preload("res://scenes/cactus.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	
 	#print("TEST")
-	#for i in range (50):
-		#var init = cactus.instantiate()
-		#add_child(init)
-		#init.position = Vector2(randi() % 950, -(randi() % 1000 + 150))
-	 # Replace with function body.
+	for i in range (50):
+		var init = cactus.instantiate()
+		add_child(init)
+		init.position = Vector2(randi() % 950, -(randi() % 1000 + 150))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/player-handler.gd
+++ scripts/player-handler.gd
@@ -18,15 +18,15 @@ func _process(delta: float) -> void:
 
 	velocity *= friction
 	position += velocity
-	#if globals.health == 0:
-		#die()
-	#if (position.x < 40 or position.x > 930) and is_Dying == false:
-		#is_Dying = true
-		#var cod = car.instantiate()
-		#add_child(cod)
-		#cod.position = Vector2(self.position.x, self.position.y + 50)
-		#await get_tree().create_timer(0.15).timeout
-		#die()
+	if globals.health == 0:
+		die()
+	if (position.x < 40 or position.x > 930) and is_Dying == false:
+		is_Dying = true
+		var cod = car.instantiate()
+		add_child(cod)
+		cod.position = Vector2(self.position.x, self.position.y + 50)
+		await get_tree().create_timer(0.15).timeout
+		die()
 		
 func die():
 	position = Vector2(500, 500)
