f367cfadcfad6e430fd46330c5084a14b4243529
oliveboy2462 <oliveboy2462@gmail.com>
2026-08-15 14:01:17 -0700

    added player area

--- scenes/cactus.gd
+++ scenes/cactus.gd
@@ -1,11 +1,17 @@
 extends Node2D
-
-
+@onready var collider: Area2D = $Area2D
+@onready var playerArea: Area2D = $playerArea
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready() -> void: # runs once and void means it reutns nothing
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+#	collider.
+	
+	
+func _on_body_entered(body: Node2D) -> void:
+	if body == playerArea:
+		print("hi")
+		
