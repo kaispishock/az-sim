abf650d08203438f42f854b6077a0c9287fd799d
oliveboy2462 <oliveboy2462@gmail.com>
2026-08-15 14:28:18 -0700

    house and a loto f stuf

--- scenes/cactus.gd
+++ scenes/cactus.gd
@@ -1,17 +1,19 @@
 extends Node2D
 @onready var collider: Area2D = $Area2D
-@onready var playerArea: Area2D = $playerArea
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void: # runs once and void means it reutns nothing
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	collider.
+func _process(delta: float) -> void:
+	print(collider.get_overlapping_areas())
+	var overlaps = collider.get_overlapping_areas()
+	for i in overlaps:
+		if i.name == "playerArea":
+			globals.health = 0
+		#globals.health = 0
 	
 	
-func _on_body_entered(body: Node2D) -> void:
-	if body == playerArea:
-		print("hi")
+
 		
--- scripts/player-handler.gd
+++ scripts/player-handler.gd
@@ -1,5 +1,5 @@
 extends Node2D
-@export var speed: int = 20
+@export var speed: int = 5
 @export var friction: float = 0.8
 @export var velocity: Vector2
 # Called when the node enters the scene tree for the first time.
@@ -12,3 +12,9 @@ func _process(delta: float) -> void:
 	velocity += Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * speed
 	velocity *= friction
 	position += velocity
+	if globals.health == 0:
+		die()
+		
+func die():
+	position = Vector2(500, 500)
+	globals.health = 100
