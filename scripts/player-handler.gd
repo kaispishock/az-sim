f8ccc9d68604b713929d5b70332b8d6c35e3a185
Corruptsive <gabriel.r.hein@outlook.com>
2026-08-15 06:55:27 -0700

    fixes

--- scripts/player-handler.gd
+++ scripts/player-handler.gd
@@ -1,12 +1,9 @@
 extends Node2D
-@export var speed: int = 10
+@export var speed: int = 5
 @export var friction: float = 0.8
 @export var velocity: Vector2
 @export var textures: AnimatedSprite2D
-<<<<<<< HEAD
-=======
 
->>>>>>> 7174af721347cdd560abe53ba1b6a280d087de6b
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
